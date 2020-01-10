c0bffc9916460037fc70e666f803e6d42799f0cf
chritchens <chritchens@gmail.com>
2020-01-10 01:37:31 +0100

    Add more types to the http module

--- src/http.rs
+++ src/http.rs
@@ -1,16 +1,52 @@
 //! `http` contains the types and functions for the HTTP API of the Liquid Network hub.
 
-use chrono::{DateTime, Utc};
+pub mod error {
+    use std::collections::HashMap;
 
-pub mod admission {
-    pub struct Admission {
-        pub address: String, // NB: [1..40] chars
-        pub authorization: String, // NB: [1..130] chars
-        pub token: String, // NB: [1..42] chars
+    pub struct ErrorData {
+        pub code: Vec<String>,
+        pub message: String,
+    }
+
+    pub struct ErrorResponse {
+        pub non_field_errors: Vec<ErrorData>,
+        pub others: HashMap<String, String>,
     }
 }
 
+pub mod common {
+    pub struct Block {
+        pub block: u64,
+        pub eon_number: u64,
+    }
+}
+
+pub mod admission {
+    pub struct Admission {
+        pub address: String,
+        pub authorization: String,
+        pub token: String,
+    }
+
+    pub const ADMISSION_CREATE_ERROR_CODES: &'static [&'static str] = &[
+        "TOKEN_NOT_REGISTERED",
+        "WALLET_BLACKLISTED",
+        "WALLET_ALREADY_ADMITTED",
+        "INVALID_ADMISSION_SIGNATURE",
+    ];
+
+    pub const ADMISSION_CREATE_BULK_ERROR_CODES: &'static [&'static str] = &[
+        "TOO_MANY_ADMISSION_REQUESTS",
+        "TOKEN_NOT_REGISTERED",
+        "INVALID_ADMISSION_SIGNATURE",
+    ];
+}
+
 pub mod analytics {
+    use chrono::{DateTime, Utc};
+
+    use crate::http::common::Block;
+
     pub struct EonSnapshot {
         pub count: u64,
         pub eon_number: u64,
@@ -18,7 +54,7 @@ pub mod analytics {
 
     pub struct DaySnapshot {
         pub count: u64,
-        pub day: DateTime<Utc>, // NB: fetched as string
+        pub day: DateTime<Utc>,
     }
 
     pub struct Challenges {
@@ -33,16 +69,11 @@ pub mod analytics {
         pub total: u64,
     }
 
-    pub struct ConfirmedBlock {
-        pub block: u64,
-        pub eon_number: u64,
-    }
-
     pub struct OperatorStatus {
         pub blocks_per_eon: u64,
-        pub confirmed: Vec<ConfirmedBlock>,
+        pub confirmed: Vec<Block>,
         pub current_eon_number: u64,
-        pub latest: ConfirmedBlock,
+        pub latest: Block,
     }
 
     pub struct Transfers {
@@ -64,16 +95,19 @@ pub mod analytics {
 }
 
 pub mod audit {
+    use crate::http::common::Block;
+    use crate::utils::URI;
+
     pub struct Blocks {
-        pub confirmed: Vec<ConfirmedBlock>,
-        pub latest: ConfirmedBlock,
+        pub confirmed: Vec<Block>,
+        pub latest: Block,
     }
 
     pub struct SwapAudit {
-        pub amount: f64, // NB: as string when fetched
-        pub amount_swapped: f64, // NB: as string when fetched
-        pub remaining_in: f64, // NB: as string when fetched
-        pub remaining_out: f64, // NB: as string when fetched
+        pub amount: f64,
+        pub amount_swapped: f64,
+        pub remaining_in: f64,
+        pub remaining_out: f64,
     }
 
     pub struct TokenOrderBook {
@@ -81,21 +115,35 @@ pub mod audit {
         pub sell_orders: Vec<SwapAudit>,
     }
 
-    // NB: only whitelisted IPs can access this data
     pub struct TokenMatching {
-        pub price: f64, // NB: as string when fetched
+        pub price: f64,
         pub time: u64,
-        pub volume: f64, // NB: as string when fetched
+        pub volume: f64,
     }
 
     pub struct Token {
-        pub address: String, // NB: [1..40] chars
-        pub name: Option<String>, // NB: <= 40 chars
-        pub short_name: Option<String>, // NB: <= 40 chars
+        pub address: String,
+        pub name: Option<String>,
+        pub short_name: Option<String>,
     }
 
-    pub struct Transfers {
+    pub struct ConciseTransfer {
+        pub amount: f64,
+        pub amount_swapped: f64,
+        pub cancelled: bool,
+        pub complete: bool,
+        pub eon_number: i64,
+        pub id: i64,
+        pub nonce: u64,
+        pub passive: bool,
         // TODO
+    }
+
+    pub struct Transfers<'a> {
+        pub count: i64,
+        pub next: Option<URI<'a>>,
+        pub previous: Option<URI<'a>>,
+        pub results: Vec<ConciseTransfer>,
     }
 
     pub struct Transfer {
@@ -112,15 +160,17 @@ pub mod audit {
 }
 
 pub mod sla {
+    use chrono::{DateTime, Utc};
+
     pub struct SLA {
-        pub cost: f64, // NB: as string when fetched
-        pub limit: u64, // NB: limit >= 1
-        pub recipient: String, // NB: [1..42] chars
-        pub token: String, // NB: [1..42] chars
+        pub cost: f64,
+        pub limit: u64,
+        pub recipient: String,
+        pub token: String,
     }
 
     pub struct WalletSLA {
-        pub expiry: DateTime<Utc>, // NB: fetched as string
+        pub expiry: DateTime<Utc>,
     }
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,10 @@
 //! `utils` contains the utility types and functions used throughout libliquefy.
 
+use uriparse;
 use uuid::Uuid;
 
 /// `UUID` is a UUID value of no specified version.
 pub struct UUID(Uuid);
+
+/// `URI` is an RFC3986 URI.
+pub struct URI<'a>(uriparse::URI<'a>);
